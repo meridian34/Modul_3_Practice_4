e78f7ff7d27660d794c16d7aeda7e375441788fa
Sobol Andrey <sobol.andrey.25@gmail.com>
2021-04-05 11:28:28 +0300

    add ILoggerService

---
+++ Modul_3_Practice_4/Model/Enum/LogType.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_3_Practice_4.Model.Enum
+{
+    public enum LogType
+    {
+        /// <summary>
+        /// This value indicates an application error.
+        /// </summary>
+        Error,
+
+        /// <summary>
+        /// This value indicates reference info.
+        /// </summary>
+        Info,
+
+        /// <summary>
+        /// This value indicates an application warning.
+        /// </summary>
+        Warning
+    }
+}
---
+++ Modul_3_Practice_4/Services/Abstract/ILoggerService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modul_3_Practice_4.Model.Enum;
+
+namespace Modul_3_Practice_4.Services.Abstract
+{
+    public interface ILoggerService
+    {
+        public void Log(LogType logType, string message);
+        public void LogInfo(string message);
+        public void LogWarning(string message);
+        public void LogError(string message);
+    }
+}
