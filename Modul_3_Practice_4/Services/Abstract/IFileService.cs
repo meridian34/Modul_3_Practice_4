65f88975c2c00dcb478ac5e16af1690b64b24b13
Sobol Andrey <sobol.andrey.25@gmail.com>
2021-04-05 13:15:09 +0300

    add file service interface

---
+++ Modul_3_Practice_4/Services/Abstract/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul_3_Practice_4.Services.Abstract
+{
+    public interface IFileService
+    {
+        public void WriteToFile(string message, string path);
+        public void CopyFile(string pathFile, string path);
+    }
+}
